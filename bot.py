e78d31a84b0c3abb62b0d81f6bcf6f25ea830571
Tri Ho <vinhocent@gmail.com>
2023-11-15 18:23:35 -0500

    added deletion

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ async def send_message(message,user_message,is_private):
     try:
         response = responses.handle_response(user_message)
         if (response != None):
+            message.delete()
             await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e:
         print(e)
