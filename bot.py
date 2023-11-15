88c9979a0fabd3c0628d251fd86c10386eda5b0c
Tri Ho <vinhocent@gmail.com>
2023-11-15 17:34:55 -0500

    removed old token

--- bot.py
+++ bot.py
@@ -11,7 +11,6 @@ async def send_message(message,user_message,is_private):
 
 
 def run_discord_bot():
-    TOKEN = 'MTE3NDQ0NjcyOTg2MTUzMzc1Nw.GfB2nA.QuFRajoz5LSpW2GbY1tGUrvYOZTF7C6OT0_AP8'
     
     intents = discord.Intents.default()
     intents.message_content = True
