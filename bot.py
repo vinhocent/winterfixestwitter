655a65d9519cc4606b0708553713be9136f871b6
Tri Ho <vinhocent@gmail.com>
2023-11-19 16:29:31 -0500

    added server id

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ def run_discord_bot():
     intents.message_content = True
     client = discord.Client(intents=intents)
     tree = app_commands.CommandTree(client)
-    @tree.command(name = "commandname", description = "My first application Command", guild=discord.Object(id=12417128931)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+    @tree.command(name = "commandname", description = "My first application Command", guild=discord.Object(id=745870568762638338)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
     async def first_command(interaction):
         await interaction.response.send_message("Hello!")
 
