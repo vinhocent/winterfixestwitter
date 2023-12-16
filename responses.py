deecdd5fd10e2eea7800c0315d51d1a71321974e
Tri Ho <vinhocent@gmail.com>
2023-12-16 03:10:57 -0500

    refactored response code

--- responses.py
+++ responses.py
@@ -1,30 +1,42 @@
 import re
+
+urlReplaceDict = [
+    "x.com/": "fxtwitter.com/",
+    "twitter.com/": "fxtwitter.com/",
+    "instagram.com/": "ddinstagram.com/"
+    "tiktok.com/": "vxtiktok.com/"
+    "pixiv.net/": "phixiv.net/"
+]
+
 def handle_response(message, author) -> str:
-    if 'https://x.com/' in message:
-        # tweet = re.search("(?P<url>https?://[^\s]+)", p_message).group("url")
-        reply = message.replace('https://x.com', 'https://fxtwitter.com')
-        return author.mention+ ": " + reply
-    
-    if 'https://twitter.com/' in message:
-        # tweet = re.search("(?P<url>https?://[^\s]+)", p_message).group("url")
-        reply = message.replace('https://twitter.com', 'https://fxtwitter.com')
-        return author.mention+ ": " + reply
-    
-    if 'instagram.com/' in message:
-        # tweet = re.search("(?P<url>https?://[^\s]+)", p_message).group("url")
-        reply = message.replace('instagram.com', 'ddinstagram.com')
-        return author.mention+ ": " + reply
+    # if 'https://x.com/' in message:
+    #     reply = message.replace('https://x.com', 'https://fxtwitter.com')
+    #     return author.mention+ ": " + reply
+    #
+    # if 'https://twitter.com/' in message:
+    #     reply = message.replace('https://twitter.com', 'https://fxtwitter.com')
+    #     return author.mention+ ": " + reply
+    #
+    # if 'instagram.com/' in message:
+    #     # tweet = re.search("(?P<url>https?://[^\s]+)", p_message).group("url")
+    #     reply = message.replace('instagram.com', 'ddinstagram.com')
+    #     return author.mention+ ": " + reply
+    #
+    #
+    # if 'tiktok.com/' in message:
+    #     reply = message.replace('tiktok.com', 'vxtiktok.com')
+    #     return author.mention+ ": " + reply
+    #
+    #
+    #
+    # if 'pixiv.net/' in message:
+    #     reply = message.replace('pixiv.net', 'phixiv.net')
+    #     return author.mention+ ": " + reply
+    for originalUrl in urlReplaceDict:
+        if originalUrl in message:
+            reply = message.replace(originalUrl, urlReplaceDict[originalUrl])  
+            return author.mention+ ": " + reply
 
-    
-    if 'tiktok.com/' in message:
-        reply = message.replace('tiktok.com', 'vxtiktok.com')
-        return author.mention+ ": " + reply
-
-
-
-    if 'pixiv.net/' in message:
-        reply = message.replace('pixiv.net', 'phixiv.net')
-        return author.mention+ ": " + reply
     if message == "!winter":
         return "I'm Winter byum blum buh"
 
