1fda9f81aa668b2f847aec6382fa0af46d91f64e
Tri Ho <vinhocent@gmail.com>
2023-12-19 14:52:16 -0500

    added async to slash commands

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord import app_commands
 from discord.utils import get
 import os
 import responses
-
+import asyncio
 async def send_message(message,user_message,is_private):
     try:
         response,needDelete = responses.handle_response(user_message,message.author)
@@ -29,6 +29,7 @@ async def addRemoveSink(message, user : discord.Member):
     except Exception as e:
         print(e)
 def run_discord_bot():
+
     
     intents = discord.Intents.default()
     intents.message_content = True
@@ -39,7 +40,7 @@ def run_discord_bot():
         await interaction.response.send_message("Hello!")
     @tree.command(name = "connectstats", description = "Get your connections Stats!")   
     async def getConnStats(interaction):
-        
+        await asyncio.sleep(4) 
         response,needDelete = responses.handle_response("!connStats",interaction.user)
         await interaction.response.send_message(response)
 
